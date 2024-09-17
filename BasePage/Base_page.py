1fd2f3717fee0dc1a7b5d981687a5a6760793833
Farhat <f190886@mail.ru>
2024-09-17 21:57:10 +0400

    add project

--- BasePage/Base_page.py
+++ BasePage/Base_page.py
@@ -3,6 +3,6 @@ from selenium.webdriver import Keys
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
-import random
+
 
 
